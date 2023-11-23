35409a37f53e610dc9e13b09e4c1e752e64c780d
maheshRathod <mahesh956154@gmail.com>
2023-11-23 20:59:18 +0530

    fixbugs

--- models/contentEpi.js
+++ models/contentEpi.js
@@ -58,8 +58,3 @@ const contentEpiSchema = Schema({
 });
 
 module.exports = mongoose.model("contentEpisode", contentEpiSchema);
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 38a1879b4f61c2e35ae8e2c2fd50bb66b69d5211
